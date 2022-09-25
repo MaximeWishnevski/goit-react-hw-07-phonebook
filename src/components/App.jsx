05fa5cbdead139ac54835400222e7ebd03f5c9ab
MaximeWishnevski <velolivefan@gmail.com>
2022-09-25 14:59:53 +0300

    app.jsx update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,63 +1,30 @@
-import { nanoid } from 'nanoid';
+import { useEffect } from 'react';
 import { Container, Title, ContcTitle, Section } from './Phonebook.styled';
 import { ContactForm } from './contactForm/ContactForm';
 import { Filter } from './filter/Filter';
 import { ContactList } from './contactList/ContactList';
-import { useContacts } from 'redux/Slices'
+import { useDispatch } from 'react-redux';
+import { fetchContacts } from '../redux/ContactsOperation';
 
 // const LS_KEY = 'contacts';
 export function App() {
-  const { contacts, filter, addNewContact, filterContacts, deleteContact } =
-    useContacts();
+  const dispatch = useDispatch();
 
-  const handleSubmit = (values, { resetForm }) => {
-    if (
-      contacts.find(
-        contact =>
-          contact.name.toLowerCase().trim() === values.name.toLowerCase().trim()
-      )
-    ) {
-      alert(`${values.name} is already in contacts`);
-      return;
+  useEffect(() => {
+    async function asyncContacts() {
+      dispatch(fetchContacts());
     }
+    asyncContacts();
+  }, [dispatch]);
 
-    const newContact = {
-      id: nanoid(5),
-      name: values.name,
-      number: values.number,
-    };
-
-    addNewContact(newContact);
-
-    resetForm();
-  };
-
-  const onChangeFilter = evt => {
-    filterContacts(evt.currentTarget.value);
-  };
-
-  const findContact = () => {
-    return contacts.filter(contact =>
-      contact.name.toLowerCase().trim().includes(filter.toLowerCase().trim())
-    );
-  };
-
-  const handleDeleteContact = id => {
-    deleteContact(id);
-  };
-
-  const results = findContact();
   return (
     <Container>
       <Section>
         <Title>Phonebook</Title>
-        <ContactForm onHandleSubmit={handleSubmit} />
+        <ContactForm />
         <ContcTitle>Contacts</ContcTitle>
-        <Filter filter={filter} onChangeFilter={onChangeFilter} />
-        <ContactList
-          contacts={results}
-          handleDeleteContact={handleDeleteContact}
-        />
+        <Filter />
+        <ContactList />
       </Section>
     </Container>
   );
